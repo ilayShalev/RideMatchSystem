1168183aa81f8b9f92740ca222a8a0fd6c46198d
Ilay Shalev <ilay53030@gmail.com>
2025-04-14 16:07:16 +0300

    Add project files.

---
+++ RideMatchSystem/Data/Models/StopDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RideMatchSystem.Data.Models
+{
+    internal class StopDetail
+    {
+    }
+}
---
+++ RideMatchSystem/UI/Dialogs/VehicleEditDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RideMatchSystem.UI.Dialogs
+{
+    public partial class VehicleEditDialog : Form
+    {
+        public VehicleEditDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
